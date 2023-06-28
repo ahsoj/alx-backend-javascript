a8e2e8d56fce700c203cd92cc317444aa4d64847
eyasu <daa282386@gmail.com>
2023-06-28 15:55:03 +0300

    ES6_promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,12 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName, lastName, fileName) {
-    const res = [];
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const res = [];
   try {
     const user = await signUpUser(firstName, lastName);
     res.push({ status: 'fulfilled', value: user });
@@ -10,7 +14,7 @@ export default async function handleProfileSignup (firstName, lastName, fileName
   } catch (err) {
     res.push({
       status: 'rejected',
-      value: Error(`${fileName} cannot be processed`)
+      value: `Error: ${fileName} cannot be processed`,
     });
   }
   return res;
