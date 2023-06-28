c9eb47e9d9bddb92c2559748c34835e6be339401
eyasu <daa282386@gmail.com>
2023-06-28 15:00:18 +0300

    ES6_promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      throw new Error();
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
     });
-  console.log('Got a response from the API');
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  try {
-    uploadPhoto().then((resp) => {
-      createUser().then((res) =>
-        console.log(resp.body, res.firstName, res.lastName)
-      );
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
-  } catch (err) {
-    console.log('Signup system offline');
-  }
 }
