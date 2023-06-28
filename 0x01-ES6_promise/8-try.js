ad8f74e121e81a19373cb55629fe4864457b47fc
eyasu <daa282386@gmail.com>
2023-06-28 16:30:34 +0300

    ES6_promise

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,5 +1,5 @@
 export default function divideFunction(numerator, denominator) {
   const value = numerator / denominator;
-  if (value === Infinity) throw new RangeError('cannot divide by 0');
+  if (value === Infinity) throw new Error('cannot divide by 0');
   return value;
 }
