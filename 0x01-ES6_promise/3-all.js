31fd30a8a30e219463307b2f33c1f48f890143c7
eyasu <daa282386@gmail.com>
2023-06-28 15:22:13 +0300

    ES6_promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  const create = createUser();
+  const upload = uploadPhoto();
+  return Promise.all([create, upload])
     .then((result) => {
       console.log(
-        `${result[0].body} ${result[1].firstName} ${result[1].lastName}`
+        `${result[1].body} ${result[0].firstName} ${result[0].lastName}`
       );
     })
     .catch(() => {
