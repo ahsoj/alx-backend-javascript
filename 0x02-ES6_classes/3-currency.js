e7d7401ec1c472f375b90917f09849a5aca3fe24
eyasu <daa282386@gmail.com>
2023-06-29 17:13:06 +0300

    ES6_classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,23 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
