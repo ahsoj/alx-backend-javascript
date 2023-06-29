ca4629096745176f23430c92f422efb0c923b9d2
eyasu <daa282386@gmail.com>
2023-06-29 17:26:49 +0300

    ES6_classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+  set amount(value) {
+    this._amount = value;
+  }
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
