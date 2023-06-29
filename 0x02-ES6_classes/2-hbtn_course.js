46eecb81b2cfcf26e8869e1693198fcb5fdba2fe
eyasu <daa282386@gmail.com>
2023-06-29 17:03:43 +0300

    ES6_classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,15 +8,18 @@ export default class HolbertonCourse {
     return this._name;
   }
   set name(value) {
-    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
   get length() {
     return this._length;
   }
   set length(value) {
-    if (typeof value !== 'number')
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
   get students() {
@@ -29,8 +32,9 @@ export default class HolbertonCourse {
     const isallString = arr.every((element) => {
       return typeof element === 'string';
     });
-    if (!isallString)
+    if (!isallString) {
       throw new TypeError('Students must be array of a strings');
+    }
     this._students = arr;
   }
 }
