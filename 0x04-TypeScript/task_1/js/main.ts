bd2d92b4e1cdc0b699a07fff2cb71d435e45f243
eyasu <daa282386@gmail.com>
2023-07-05 19:26:03 +0300

    0x04-Typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,42 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index: string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName, lastName) =>
+  `${firstName[0]}. ${lastName}`;
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+}
