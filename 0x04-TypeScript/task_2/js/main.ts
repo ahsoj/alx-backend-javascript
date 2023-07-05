7e9e38f89c89e53120eeb61f8badf7463f9c0536
eyasu <daa282386@gmail.com>
2023-07-05 20:47:52 +0300

    0x04-Typescript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -41,3 +41,13 @@ export function executeWork(employee: Director | Teacher) {
     return (employee as Teacher).workTeacherTasks();
   }
 }
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
